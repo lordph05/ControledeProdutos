574d83416312c440a03ed34b4e408cf6421af2cb
Paulo Henrique <101574096+lordph05@users.noreply.github.com>
2022-08-27 12:51:03 -0300

    quasefinalizado

---
+++ app/src/main/java/com/example/controledeprodutos/adapter/AdapterProduto.java
@@ -0,0 +1,66 @@
+package com.example.controledeprodutos.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.controledeprodutos.R;
+import com.example.controledeprodutos.model.Produto;
+
+import java.util.List;
+
+public class AdapterProduto  extends RecyclerView.Adapter<AdapterProduto.MyViewHolder> {
+
+    private List <Produto> produtoList;
+    private  OnClick onClick;
+
+    public AdapterProduto(List<Produto> produtoList, OnClick onClick) {
+        this.produtoList = produtoList;
+        this.onClick = onClick;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View intemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_produto, parent,false);
+        return new MyViewHolder(intemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+Produto produto = produtoList.get(position);
+
+holder.textProduto.setText(produto.getNome());
+holder.textEstoque.setText("EStoque: "+String.valueOf(produto.getEstoque()));
+        holder.textValor.setText("R$: "+String.valueOf(produto.getValor()));
+
+        holder.itemView.setOnClickListener(view -> onClick.onClickListener(produto));
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return produtoList.size();
+    }
+
+    public interface OnClick{
+        void onClickListener (Produto produto);
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView textProduto, textEstoque, textValor;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textProduto = itemView.findViewById(R.id.text_produto);
+            textEstoque = itemView.findViewById(R.id.text_estoque);
+            textValor = itemView.findViewById(R.id.text_valor);
+
+        }
+    }
+}
